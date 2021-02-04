a5e05bff42a718a25f4205908d132585a79c20a4
xocink <57539437+xocink@users.noreply.github.com>
2021-02-04 19:42:52 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,24 @@
 
-// You should implement your task here.
+exports.min = function min(array = 1) {
+    if (arguments.length == 0) return 0
+    if (Array.isArray(array) && array.length == 0) return 0;
+    return Math.min(...array)
+}
 
-module.exports = function towelSort (matrix) {
-  return [];
+
+exports.max = function max(array) {
+    if (arguments.length == 0) return 0;
+    if (Array.isArray(array) && array.length == 0) return 0;
+    if (array == undefined) return 0;
+    return Math.max(...array);
+}
+
+exports.avg = function avg(array) {
+    if (arguments.length == 0) return 0;
+    if (Array.isArray(array) && array.length == 0) return 0;
+    if (array == undefined) return 0;
+    let value = array.reduce(function (prev, curr) {
+        return (prev + curr)
+    });
+    return value / array.length;
 }
